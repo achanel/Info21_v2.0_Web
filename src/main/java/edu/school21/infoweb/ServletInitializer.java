621a747ac6b69bb5b9b2e0a1ee04d96b14140062
achanel <x@x>
2023-10-15 11:56:02 +0300

    tests fixes

--- src/main/java/edu/school21/infoweb/ServletInitializer.java
+++ src/main/java/edu/school21/infoweb/ServletInitializer.java
@@ -4,9 +4,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(InfoWeb2Application.class);
     }
 }
